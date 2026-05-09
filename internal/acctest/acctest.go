9b9a62bd2881df62f5e72bc669c1c8d49a3bd020
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-09 12:26:15 -0700

    Fix errcheck lint on acctest helper
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- internal/acctest/acctest.go
+++ internal/acctest/acctest.go
@@ -19,7 +19,7 @@ func WithVersionEndpoint(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet && r.URL.Path == "/api/v1/version" {
 			w.Header().Set("Content-Type", "application/json")
-			w.Write([]byte(`"4.0.0-test"`))
+			_, _ = w.Write([]byte(`"4.0.0-test"`))
 			return
 		}
 		next.ServeHTTP(w, r)
