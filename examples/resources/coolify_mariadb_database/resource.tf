0b9cfbc980c66aecf9219128d450245a951f198a
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-09 08:11:07 -0700

    Fix HCL formatting in 5 example files
    terraform fmt -recursive on examples/ fixed indentation in:
    - coolify_docker_image_application
    - coolify_mariadb_database
    - coolify_mongodb_database
    - coolify_mysql_database
    - coolify_postgresql_database
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- examples/resources/coolify_mariadb_database/resource.tf
+++ examples/resources/coolify_mariadb_database/resource.tf
@@ -1,10 +1,10 @@
 resource "coolify_mariadb_database" "example" {
-  name                 = "my-mariadb"
-  project_uuid         = coolify_project.example.uuid
-  server_uuid          = coolify_server.example.uuid
-  image                = "mariadb:11"
-  mariadb_user         = "app"
-  mariadb_password     = "secret"
-  mariadb_database     = "myapp"
+  name                  = "my-mariadb"
+  project_uuid          = coolify_project.example.uuid
+  server_uuid           = coolify_server.example.uuid
+  image                 = "mariadb:11"
+  mariadb_user          = "app"
+  mariadb_password      = "secret"
+  mariadb_database      = "myapp"
   mariadb_root_password = "rootsecret"
 }
