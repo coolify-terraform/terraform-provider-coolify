eb791da398d7bb40f484146de02e2d8433c2a780
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-13 17:15:54 -0700

    test: add direct unit tests for normalizeGitRepository
    Table-driven test covering all 5 branches of the normalization logic:
    full URL (noop), SSH URL (noop), domain-prefixed (noop), bare slug
    (GitHub prefix added), empty/no-slash (noop). Catches regressions
    the integration tests cannot isolate.
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

---
+++ internal/service/application/common_test.go
@@ -0,0 +1,31 @@
+package application
+
+import "testing"
+
+func TestNormalizeGitRepository(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name string
+		in   string
+		want string
+	}{
+		{"full https URL", "https://github.com/org/repo", "https://github.com/org/repo"},
+		{"full http URL", "http://github.com/org/repo", "http://github.com/org/repo"},
+		{"SSH URL", "git@github.com:org/repo.git", "git@github.com:org/repo.git"},
+		{"domain-prefixed", "github.com/org/repo", "github.com/org/repo"},
+		{"gitlab domain", "gitlab.com/org/repo", "gitlab.com/org/repo"},
+		{"bare slug", "org/repo", "https://github.com/org/repo"},
+		{"bare slug nested", "org/repo/subdir", "https://github.com/org/repo/subdir"},
+		{"empty string", "", ""},
+		{"single word", "myrepo", "myrepo"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			got := normalizeGitRepository(tt.in)
+			if got != tt.want {
+				t.Errorf("normalizeGitRepository(%q) = %q, want %q", tt.in, got, tt.want)
+			}
+		})
+	}
+}
