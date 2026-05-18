e95bdf0b00de743ad99befae183b73bcc6d92409
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-18 15:38:43 -0700

    chore: update scaffold template to use flex.ConfigureClient
    R2 New Contributor perspective: the scaffold emitted the old 12-line
    manual type assertion in Configure. Updated to the canonical one-liner
    so new resources start with the correct pattern.
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- scripts/new-resource.sh
+++ scripts/new-resource.sh
@@ -185,18 +185,7 @@ func (r *${LOWER}Resource) Schema(_ context.Context, _ resource.SchemaRequest, r
 }
 
 func (r *${LOWER}Resource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
-	if req.ProviderData == nil {
-		return
-	}
-	c, ok := req.ProviderData.(*client.Client)
-	if !ok {
-		resp.Diagnostics.AddError(
-			"Unexpected Resource Configure Type",
-			fmt.Sprintf("Expected *client.Client, got: %T", req.ProviderData),
-		)
-		return
-	}
-	r.client = c
+	r.client = flex.ConfigureClient(req, &resp.Diagnostics)
 }
 
 func (r *${LOWER}Resource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
