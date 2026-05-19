865fe6ef0973c5b0dab20a327d51225b19a981a9
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-19 04:17:09 -0700

    Add storage import UUID validation tests
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- internal/service/storage/resource_test.go
+++ internal/service/storage/resource_test.go
@@ -373,6 +373,82 @@ func TestStorageResource_ImportBadType(t *testing.T) {
 	})
 }
 
+func TestStorageResource_ImportBadParentUUID(t *testing.T) {
+	t.Parallel()
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/v1/applications/{appUUID}/storages", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]string{"uuid": "stor-err3-uuid"})
+	})
+	mux.HandleFunc("GET /api/v1/applications/{appUUID}/storages", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string][]client.Storage{"persistent_storages": {{UUID: "stor-err3-uuid", Name: "n", MountPath: "/m"}}, "file_storages": {}})
+	})
+	mux.HandleFunc("DELETE /api/v1/applications/{appUUID}/storages/{storUUID}", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+	})
+	srv := httptest.NewServer(acctest.WithVersionEndpoint(mux))
+	defer srv.Close()
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: testStorageResourceConfig(srv.URL, `
+					application_uuid = "cccc0001-0001-4000-8000-000000000001"
+					name             = "n"
+					mount_path       = "/m"
+				`),
+			},
+			{
+				ResourceName:  "coolify_storage.test",
+				ImportState:   true,
+				ImportStateId: "application:not-a-uuid:bbbb0001-0001-4000-8000-000000000001",
+				ExpectError:   regexp.MustCompile(`(?s)Invalid Import ID.*parent UUID segment`),
+			},
+		},
+	})
+}
+
+func TestStorageResource_ImportBadStorageUUID(t *testing.T) {
+	t.Parallel()
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/v1/applications/{appUUID}/storages", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]string{"uuid": "stor-err4-uuid"})
+	})
+	mux.HandleFunc("GET /api/v1/applications/{appUUID}/storages", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string][]client.Storage{"persistent_storages": {{UUID: "stor-err4-uuid", Name: "n", MountPath: "/m"}}, "file_storages": {}})
+	})
+	mux.HandleFunc("DELETE /api/v1/applications/{appUUID}/storages/{storUUID}", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+	})
+	srv := httptest.NewServer(acctest.WithVersionEndpoint(mux))
+	defer srv.Close()
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: testStorageResourceConfig(srv.URL, `
+					application_uuid = "cccc0001-0001-4000-8000-000000000001"
+					name             = "n"
+					mount_path       = "/m"
+				`),
+			},
+			{
+				ResourceName:  "coolify_storage.test",
+				ImportState:   true,
+				ImportStateId: "application:aaaa0001-0001-4000-8000-000000000001:not-a-uuid",
+				ExpectError:   regexp.MustCompile(`(?s)Invalid Import ID.*storage UUID segment`),
+			},
+		},
+	})
+}
+
 // ---------------------------------------------------------------------------
 // TestStorageResource_CreateWithServiceUUID
 // ---------------------------------------------------------------------------
