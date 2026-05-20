7c5367fce61d9a94f934f18888064d473200c9d4
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-20 01:08:07 -0700

    Add apisettings Update test (toggle enabled true->false)
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- internal/service/apisettings/resource_test.go
+++ internal/service/apisettings/resource_test.go
@@ -46,6 +46,42 @@ func TestAPISettingsResource_Enable(t *testing.T) {
 	}
 }
 
+func TestAPISettingsResource_Update(t *testing.T) {
+	t.Parallel()
+	var enabled atomic.Bool
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/v1/enable", func(w http.ResponseWriter, _ *http.Request) {
+		enabled.Store(true)
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`{"message":"API enabled."}`))
+	})
+	mux.HandleFunc("GET /api/v1/disable", func(w http.ResponseWriter, _ *http.Request) {
+		enabled.Store(false)
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`{"message":"API disabled."}`))
+	})
+	srv := httptest.NewServer(acctest.WithVersionEndpoint(mux))
+	defer srv.Close()
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: acctest.TestResourceConfig(srv.URL, "coolify_api_settings", "test", `
+					enabled = true
+				`),
+				Check: resource.TestCheckResourceAttr("coolify_api_settings.test", "enabled", "true"),
+			},
+			{
+				Config: acctest.TestResourceConfig(srv.URL, "coolify_api_settings", "test", `
+					enabled = false
+				`),
+				Check: resource.TestCheckResourceAttr("coolify_api_settings.test", "enabled", "false"),
+			},
+		},
+	})
+}
+
 func TestAPISettingsResource_Disable(t *testing.T) {
 	t.Parallel()
 	mux := http.NewServeMux()
