cb67ab5f45c99578c561fd67f86fc9ef3587c947
Sebastien Tardif <sebtardif@ncf.ca>
2026-05-24 17:56:29 -0700

    fix(scenario): mark env-scale outputs as sensitive
    The api_env_count and worker_env_count outputs compute length() over
    the envs_bulk variables map, which contains internal_db_url (a
    sensitive value). Terraform requires outputs derived from sensitive
    values to be marked sensitive = true.
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- examples/scenarios/acme-env-scale/outputs.tf
+++ examples/scenarios/acme-env-scale/outputs.tf
@@ -1,23 +1,31 @@
 output "project_uuid" {
-  value = coolify_project.env_scale.uuid
+  description = "UUID of the project"
+  value       = coolify_project.env_scale.uuid
 }
 
 output "database_uuid" {
-  value = coolify_database_postgresql.shared.uuid
+  description = "UUID of the shared PostgreSQL database"
+  value       = coolify_database_postgresql.shared.uuid
 }
 
 output "api_uuid" {
-  value = coolify_application_docker_image.api.uuid
+  description = "UUID of the API application"
+  value       = coolify_application_docker_image.api.uuid
 }
 
 output "worker_uuid" {
-  value = coolify_application_docker_image.worker.uuid
+  description = "UUID of the Worker application"
+  value       = coolify_application_docker_image.worker.uuid
 }
 
 output "api_env_count" {
-  value = length(coolify_envs_bulk.api.variables)
+  description = "Number of environment variables set on the API application"
+  value       = length(coolify_envs_bulk.api.variables)
+  sensitive   = true
 }
 
 output "worker_env_count" {
-  value = length(coolify_envs_bulk.worker.variables)
+  description = "Number of environment variables set on the Worker application"
+  value       = length(coolify_envs_bulk.worker.variables)
+  sensitive   = true
 }
