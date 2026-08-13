0ab2b4ee6d5ae7ad06cd4d5fe78d1b384445534e
Sebastien Tardif <sebtardif@ncf.ca>
2026-08-12 23:12:13 -0700

    test: add update step to pushover notification acc test
    TestAccCoverage_ResourcesHaveCRUDSteps requires >=2 Config steps.
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- internal/service/notificationpushover/resource_acc_test.go
+++ internal/service/notificationpushover/resource_acc_test.go
@@ -26,6 +26,21 @@ resource "coolify_notification_pushover" "test" {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("coolify_notification_pushover.test", "id", "current"),
 					resource.TestCheckResourceAttr("coolify_notification_pushover.test", "enabled", "false"),
+					resource.TestCheckResourceAttr("coolify_notification_pushover.test", "deployment_failure", "true"),
+				),
+			},
+			{
+				Config: acctest.ConfigProviderBlock() + `
+resource "coolify_notification_pushover" "test" {
+  enabled            = false
+  user_key           = "u-coolify-tf-acc"
+  api_token          = "a-coolify-tf-acc"
+  deployment_failure = true
+  backup_failure     = true
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("coolify_notification_pushover.test", "backup_failure", "true"),
 				),
 			},
 			{
