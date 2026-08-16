50f0fe012ff75e263a0b34e035e3aee7fbb76f9a
Sebastien Tardif <sebtardif@ncf.ca>
2026-08-16 10:10:32 -0700

    test: cover attach API error on coolify_application_destination
    Same create-failure gap as the other 4.3 attach resources. A 422 on
    POST /applications/{uuid}/destinations must surface as an attach error.
    
    Signed-off-by: Sebastien Tardif <sebtardif@ncf.ca>

--- internal/service/applicationdestination/resource_test.go
+++ internal/service/applicationdestination/resource_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"strings"
 	"sync"
 	"testing"
@@ -121,6 +122,28 @@ resource "coolify_application_destination" "test" {
 				acctest.CheckPathDisappears(srv.URL, "/api/v1/applications/"+appUUID+"/destinations/"+destUUID),
 			),
 			ExpectNonEmptyPlan: true,
+		}},
+	})
+}
+
+func TestApplicationDestinationResource_CreateAPIError(t *testing.T) {
+	t.Parallel()
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /api/v1/applications/{uuid}/destinations", func(w http.ResponseWriter, _ *http.Request) {
+		http.Error(w, `{"message":"validation failed"}`, http.StatusUnprocessableEntity)
+	})
+	srv := httptest.NewServer(acctest.WithVersionEndpoint(mux))
+	defer srv.Close()
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: acctest.TestProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{{
+			Config: acctest.ProviderBlockForURL(srv.URL) + `
+resource "coolify_application_destination" "test" {
+  application_uuid = "aaaa0001-0001-4000-8000-000000000001"
+  destination_uuid = "dddd0001-0001-4000-8000-000000000001"
+}`,
+			ExpectError: regexp.MustCompile(`Error attaching application destination`),
 		}},
 	})
 }
